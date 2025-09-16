3e23ba4dabaeb5954f6b16bf70be648ffb470436
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-09-16 10:59:17 +0700

    l

--- open.sh
+++ open.sh
@@ -6,8 +6,8 @@
 ##########################################################################
                      API="ดึงรายการข้อมูลรถฉุกเฉิน" 
                      google_sheet="https://docs.google.com/spreadsheets/d/11uBJXZQjn51m-JKFSK8h8SeAq9IbhbMRxtAlD9-8FOM/edit?gid=303761516#gid=303761516" 
-                     id="5"                 #เปลี่ยน id ทุกครั้งที่ยิง
-                     user="10000";            #จำนวนผู้ใช้งาน
+                     id="6"                 #เปลี่ยน id ทุกครั้งที่ยิง
+                     user="6000";            #จำนวนผู้ใช้งาน
                      duration="1";         #วินาที
                      scenario="1"           #scenario="1" ยิงเเบบกำหนด request (duration ได้แค่ 1 วินาที)
                      cid="1"                #scenario="2" ยิงเเบบกำหนด VUs  (กำหนดว่า user x คน ใช้ระบบ x วินาที)
